19e3ae2144fcd98e5df47c170a50214de0d2c693
dedli330 <103512377+dedli22@users.noreply.github.com>
2022-05-19 17:19:17 +0300

    OlegOS

---
+++ resources/views/portal_v3.blade.php
@@ -0,0 +1,29 @@
+@include('themes/puffy.header')
+
+@include('themes/puffy.userNavBar')
+
+@include('themes/puffy.userSideBar')
+
+
+<section class="centerContent">
+        
+            
+    lapas saturs
+    
+    
+    
+    </section>
+
+
+
+
+
+
+
+
+
+@include('themes/puffy.footer')
+
+
+
+
